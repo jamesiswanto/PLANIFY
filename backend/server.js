6808d628ad79de1946e9d023b37a2cf56df034ac
James Michael Siswanto <jamesmchl.18@gmail.com>
2024-11-25 11:41:03 +0700

    Update Backend's package.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL, // Ensure CLIENT_URL is set in Railway's environment variables
     credentials: true,
   })
 );
@@ -23,14 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// error handler middleware
+// Error handler middleware
 app.use(errorHandler);
 
-//routes
+// Routes
 const routeFiles = fs.readdirSync("./src/routes");
-
 routeFiles.forEach((file) => {
-  // use dynamic import
   import(`./src/routes/${file}`)
     .then((route) => {
       app.use("/api/v1", route.default);
@@ -40,15 +38,15 @@ routeFiles.forEach((file) => {
     });
 });
 
+// Start the server
 const server = async () => {
   try {
     await connect();
-
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   } catch (error) {
-    console.log("Failed to strt server.....", error.message);
+    console.log("Failed to start server:", error.message);
     process.exit(1);
   }
 };
